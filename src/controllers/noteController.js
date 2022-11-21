b231bf456586dbd5db5db2512a4d87acfd532b42
donkennie <ajeigbekehinde160@gmail.com>
2022-11-21 02:35:07 +0100

    Create a post endpoint for note

---
+++ src/controllers/noteController.js
@@ -0,0 +1,32 @@
+const { response } = require("express");
+const database = require("../database/connection")
+const response = require("../utils/responseApi")
+
+exports.CreateNewNote = async (req, res, next) => {
+    try {
+        const {
+            title,
+            description
+        } = req.body;
+
+        const {id} = req.user;
+
+        const createNote = await database.query(
+            `INSERT INTO notes (title, description) VALUES(?,?)`, 
+            [title, description]
+        );
+
+        let data = {
+            id: createNote[0].insertId,
+            title,
+            description
+        }
+
+        return response(res, 200, "Note created successfully",{
+            data,
+        })
+
+    } catch (error) {
+        next(error)
+    }
+};
