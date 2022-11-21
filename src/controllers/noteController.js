f15413bfdb5ed2601df0efc7acee48be737a4f51
donkennie <ajeigbekehinde160@gmail.com>
2022-11-21 15:15:28 +0100

    create update endpoint

--- src/controllers/noteController.js
+++ src/controllers/noteController.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const { data } = require("jquery");
 const database = require("../database/connection")
 const response = require("../utils/responseApi")
 
@@ -37,7 +38,43 @@ exports.editNote = async (req, res, next) => {
             title,
             description
         } = req.body;
+
+        const updateNote = await database.query(`UPDATE notes SET title= ?,  description = ? WHERE id = ? `, [notes.title, notes.description, id],
+        (err, res) => { 
+            if (err) { 
+              console.log("error: ", err); 
+              result(null, err); 
+              return; 
+            } 
+        });
+
+        if (res.affectedRows == 0) { 
+            // not found employee with the id 
+            result({ kind: "not_found" }, null); 
+            return; 
+          }
+
+          let data = {
+            id: updateNote[0].insertId,
+            title,
+            description,
+          }
+
+          return response(res, 201, "Updated successfully", {
+            data,
+          });
+          
+
     } catch (error) {
-        
+        next(error);
+    }
+};
+
+exports.getAllNotes = async (req, res, next) => {
+    try {
+        const notes = await database.query(`SELECT * FROM notes`);
+        return response(res, 200, "All notes", notes[0]);
+    } catch (error) {
+        next(error);
     }
 }
